3ffbdf9ba3b9a434a4279dfc5e2c2ac09df6efb6
starwort <tcphone93@gmail.com>
2021-02-26 21:49:00 +0000

    Save every 30 minutes to mitigate data loss bug If onbeforeunload was not called, the user's data would not be saved; data is now also saved every 30 minutes in case the user's browser crashes

--- docs/assets/nookdata.js
+++ docs/assets/nookdata.js
@@ -12,7 +12,13 @@ if (window.localStorage.nookdata === undefined) {
 
 var nookdata_data = JSON.parse(window.localStorage.nookdata);
 
+function save() {
+    window.localStorage.nookdata = JSON.stringify(nookdata_data);
+}
+
+window.setInterval(save, 30 * 60000);
+
 window.onbeforeunload = function (event) {
     // Save data before page unload
-    window.localStorage.nookdata = JSON.stringify(nookdata_data);
+    save();
 }
